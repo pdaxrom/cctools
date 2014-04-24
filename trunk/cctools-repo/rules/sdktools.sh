623c14be6b471d5fb0f8ec7e9bae3a8657236107
sash <sash@ffa6d455-a9f7-4004-8c58-8cc8fa5122a1>
2014-04-24 17:45:18 +0000

    cctools modules

--- trunk/cctools-repo/rules/sdktools.sh
+++ trunk/cctools-repo/rules/sdktools.sh
@@ -73,8 +73,8 @@ exec dalvikvm -Xss262912 -Xmx256M -cp \$CCTOOLSRES com.pdaxrom.cmdline.AProjectH
 EOF
     chmod 755 ${TMPINST_DIR}/${PKG}/cctools/bin/aproject-helper
 
-    mkdir -p ${TMPINST_DIR}/${PKG}/cctools/share/project
-    cp -f project/*java.xml ${TMPINST_DIR}/${PKG}/cctools/share/project/
+    mkdir -p ${TMPINST_DIR}/${PKG}/cctools/share/modules
+    cp -f modules/*java.xml ${TMPINST_DIR}/${PKG}/cctools/share/modules/
 
     local filename="${PKG}_${PKG_VERSION}_${PKG_ARCH}.zip"
     build_package_desc ${TMPINST_DIR}/${PKG} $filename ${PKG} $PKG_VERSION $PKG_ARCH "$PKG_DESC" "fastjar"

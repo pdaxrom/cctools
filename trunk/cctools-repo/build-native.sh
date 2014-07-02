4b0db6e423f979f03e7f313f0f606fa820b2d92d
sash <sash@ffa6d455-a9f7-4004-8c58-8cc8fa5122a1>
2014-07-02 07:58:26 +0000

    autoinstall compiled packages

--- trunk/cctools-repo/build-native.sh
+++ trunk/cctools-repo/build-native.sh
@@ -9,6 +9,17 @@ fi
 
 WRKDIR=${HOME}/tmp
 
+mkdir -p ${WRKDIR}/repo/armeabi
+mkdir -p ${WRKDIR}/repo/mips
+mkdir -p ${WRKDIR}/repo/x86
+test -h ${WRKDIR}/repo/armeabi-v7a || ln -sf armeabi ${WRKDIR}/repo/armeabi-v7a
+test -h ${WRKDIR}/repo/mips-r2     || ln -sf mips    ${WRKDIR}/repo/mips-r2
+
+if ! cat ${CCTOOLSDIR}/etc/repos.list | grep -q "${WRKDIR}/repo"; then
+    echo "Adding local repo to list"
+    echo -n -e "\nfile://${WRKDIR}/repo\n" >> ${CCTOOLSDIR}/etc/repos.list
+fi
+
 case `uname -m` in
 arm*)
     ash ./build-shell-utils.sh ${PWD}/src arm-linux-androideabi ${WRKDIR}/arm-repo  || exit 1

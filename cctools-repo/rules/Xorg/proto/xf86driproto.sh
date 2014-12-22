d13c5de9b888e7149083d8881b40742e3fb0f62d
sashz <sashz@pdaxrom.org>
2014-12-22 10:38:30 +0000

    updated make packages

--- cctools-repo/rules/Xorg/proto/xf86driproto.sh
+++ cctools-repo/rules/Xorg/proto/xf86driproto.sh
@@ -4,7 +4,7 @@ build_xf86driproto() {
     PKG_SUBVERSION=
     PKG_URL="http://www.x.org/releases/X11R7.7/src/proto/${PKG}-${PKG_VERSION}.tar.bz2"
     PKG_DESC="X11 DRI extension wire protocol"
-    PKG_DEPS=""
+    PKG_DEPS="xorg-util-macros-dev"
     O_FILE=$SRC_PREFIX/${PKG}/${PKG}-${PKG_VERSION}.tar.bz2
     S_DIR=$src_dir/${PKG}-${PKG_VERSION}
     B_DIR=$build_dir/${PKG}
@@ -35,18 +35,11 @@ build_xf86driproto() {
 
     $MAKE install || error "make install"
 
+    PKG=xorg-${PKG}
+
     $MAKE install prefix=${TMPINST_DIR}/${PKG}/cctools || error "package install"
 
-    if [ -d ${TMPINST_DIR}/${PKG}/cctools/lib/pkgconfig ]; then
-	sed -i "s|^prefix=.*\$|prefix=${TARGET_INST_DIR}|" ${TMPINST_DIR}/${PKG}/cctools/lib/pkgconfig/*.pc
-    fi
-
-    $TARGET_ARCH-strip ${TMPINST_DIR}/${PKG}/cctools/bin/*
-
-    local filename="xorg-${PKG}-dev_${PKG_VERSION}${PKG_SUBVERSION}_${PKG_ARCH}.zip"
-    build_package_desc ${TMPINST_DIR}/${PKG} $filename xorg-${PKG}-dev ${PKG_VERSION}${PKG_SUBVERSION} $PKG_ARCH "$PKG_DESC" "$PKG_DEPS"
-    cd ${TMPINST_DIR}/${PKG}
-    rm -f ${REPO_DIR}/$filename; zip -r9y ${REPO_DIR}/$filename *
+    make_packages nomain
 
     popd
     s_tag $PKG

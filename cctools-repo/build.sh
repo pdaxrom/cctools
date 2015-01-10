5418c1d98746de370746fdfcc5c327e582aed72a
sashz <sashz@pdaxrom.org>
2015-01-10 20:53:16 +0000

    android SDL2

--- cctools-repo/build.sh
+++ cctools-repo/build.sh
@@ -25,7 +25,7 @@ export PATH=/opt/CodeSourcery/bin:$PATH
 
 ./build-shell-utils.sh ${PWD}/src arm-linux-androideabi ${WRKDIR}/arm-repo  || exit 1
 
-exit 0
+#exit 1
 
 ./build-shell-utils.sh ${PWD}/src mipsel-linux-android  ${WRKDIR}/mips-repo || exit 1
 

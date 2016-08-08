ea0716c5326f5dc35c7042980fb015918abb121f
sashz <sashz@pdaxrom.org>
2016-08-08 12:53:14 +0000

    no clang yet

--- cctools-repo/build.sh
+++ cctools-repo/build.sh
@@ -13,23 +13,21 @@ WRKDIR=${WRKDIR}-pie
 # -------------------------------
 #
 
-NDKDIR=/mnt/devel/Work/NDK/android-ndk-r10e
-#NDKSRC=/home/sash/Work/android/ndk-source
-NDKSRC=/mnt/devel/Work/NDK/nkd-source
+SDKDIR=/home/sash/Android/android-sdk
+NDKDIR=/home/sash/Android/android-ndk
+NDKSRC=/home/sash/Android/ndk-src
 
-for d in binutils gcc gmp mpc mpfr cloog isl ppl llvm-3.5 llvm-3.6; do
+for d in binutils gcc gmp mpc mpfr cloog isl ppl llvm-3.6; do
     ln -sf ${NDKSRC}/${d} src/
 done
 
-export PATH=/opt/CodeSourcery/bin:$PATH
+export PATH=~/bin/CodeSourcery/bin:$PATH
 
-./build-shell-utils.sh ${PWD}/src arm-linux-androideabi ${WRKDIR}/arm-repo  || exit 1
+./build-shell-utils.sh ${PWD}/src arm-linux-androideabi ${WRKDIR}/arm-repo $NDKDIR $SDKDIR || exit 1
 
-#exit 1
+./build-shell-utils.sh ${PWD}/src mipsel-linux-android  ${WRKDIR}/mips-repo $NDKDIR $SDKDIR || exit 1
 
-./build-shell-utils.sh ${PWD}/src mipsel-linux-android  ${WRKDIR}/mips-repo || exit 1
-
-./build-shell-utils.sh ${PWD}/src i686-linux-android    ${WRKDIR}/i686-repo || exit 1
+./build-shell-utils.sh ${PWD}/src i686-linux-android    ${WRKDIR}/i686-repo $NDKDIR $SDKDIR || exit 1
 
 test -e ${WRKDIR}/repo/armeabi-v7a || ln -sf armeabi ${WRKDIR}/repo/armeabi-v7a
 test -e ${WRKDIR}/repo/mips-r2     || ln -sf mips    ${WRKDIR}/repo/mips-r2

23785d2a4d9beda806b489a3ae19f4f574ac2197
sashz <sashz@pdaxrom.org>
2016-09-03 05:34:46 +0000

    64 bit fix

--- cctools-repo/build-native.sh
+++ cctools-repo/build-native.sh
@@ -9,6 +9,9 @@ fi
 
 WRKDIR=${HOME}/tmp
 
+mkdir -p ${WRKDIR}/repo/aarch64
+mkdir -p ${WRKDIR}/repo/mips64el
+mkdir -p ${WRKDIR}/repo/x86_64
 mkdir -p ${WRKDIR}/repo/armeabi
 mkdir -p ${WRKDIR}/repo/mips
 mkdir -p ${WRKDIR}/repo/x86
@@ -21,14 +24,23 @@ if ! cat ${CCTOOLSDIR}/etc/repos.list | grep -q "${WRKDIR}/repo"; then
 fi
 
 case `uname -m` in
+aarch64*)
+    ash ./build-shell-utils.sh ${PWD}/src aarch64-linux-android  ${WRKDIR}/aarch64-repo  || exit 1
+    ;;
 arm*)
-    ash ./build-shell-utils.sh ${PWD}/src arm-linux-androideabi ${WRKDIR}/arm-repo  || exit 1
+    ash ./build-shell-utils.sh ${PWD}/src arm-linux-androideabi  ${WRKDIR}/arm-repo      || exit 1
+    ;;
+mips64el*)
+    ash ./build-shell-utils.sh ${PWD}/src mips64el-linux-android ${WRKDIR}/mips64el-repo || exit 1
     ;;
 mips*)
-    ash ./build-shell-utils.sh ${PWD}/src mipsel-linux-android  ${WRKDIR}/mips-repo || exit 1
+    ash ./build-shell-utils.sh ${PWD}/src mipsel-linux-android   ${WRKDIR}/mips-repo     || exit 1
+    ;;
+x86_64*)
+    ash ./build-shell-utils.sh ${PWD}/src x86_64-linux-android   ${WRKDIR}/x86_64-repo   || exit 1
     ;;
 *)
-    ash ./build-shell-utils.sh ${PWD}/src i686-linux-android    ${WRKDIR}/i686-repo || exit 1
+    ash ./build-shell-utils.sh ${PWD}/src i686-linux-android     ${WRKDIR}/i686-repo     || exit 1
     ;;
 esac
 
